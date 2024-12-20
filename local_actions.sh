09caffb6de2a12a8a566f649c4741050938d4604
dogusyuksel <dogusyuk@gmail.com>
2024-12-20 11:30:31 +0000

    removed common docker dependency and added version and changelog (#13)

---
+++ local_actions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# important!! first call must have '-b' option
+./docker_ctl.sh -b -s -c 'cd /workspace && source environment && cd firmware/ble5_simple_peripheral_cc2640r2lp_app && make -C ./FlashROM_StackLibrary/ clean && make -C ./FlashROM_StackLibrary/ all'
